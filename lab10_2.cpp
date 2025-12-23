4537365c2f032c69b4660eb96422f6f9dff55525
Ploythun <ploytwrsrijandon@gmail.com>
2025-12-23 15:38:20 +0700

    Lab10 complete

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -8,6 +8,13 @@ int main (){
 	ofstream dest;
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
+	string text;
+	
+	dest << "-------------------- BOOM ---------------------\n";
+	while(getline(source, text)){
+		cout << text << endl;
+	}
+	dest << "-------------------- HA!! ---------------------";
 	
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,32 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cmath>
+#include<iomanip>
+#include<cstdlib>
+using namespace std;
+
+int main() {
+    ifstream data;
+    data.open("score.txt");
+
+    int i = 0;
+    double sum = 0, sum2 = 0;
+    string scoretxt;
+    
+
+    while(getline(data, scoretxt)){
+        i++;
+        double score = stod(scoretxt);
+        sum = sum + score;
+        sum2 = sum2 + score*score;
+    }
+
+    double mean = sum/i;
+    double sd = sqrt((sum2/i)-(mean*mean));
+
+    cout << "Number of data = " << i << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << sd << endl;
+}
